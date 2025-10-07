30934ab086fa85938fde322a69bf96a394f7a3d9
Muresan Mihai <Muresan.Fl.Remus@student.utcluj.ro>
2025-10-07 09:40:27 +0300

    trebe sa mearga

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from idlelib.colorizer import prog_group_name_to_tag
+
 if __name__ == '__main__':
     print("Hello, World!?!")
     print("Hello, World!?!")
     print("hi, git!")
     print("Serbus")
+    print("acuma merge")
 
